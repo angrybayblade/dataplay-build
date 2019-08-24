ad8358fa848edf17468d1583d47f52a309aacc35
jithuazeez <jithuazeez@gmail.com>
2019-08-24 17:06:05 +0530

    validation updated

--- train/svr.py
+++ train/svr.py
@@ -1,11 +1,9 @@
 from sklearn.svm import SVR
 from validation import *
 
-evals={
-"root_mean_squared_error":rmse,"mean_squared_error":mse,"mean_absolute_error":mae,"r2_score":r2,"mean_squared_log_error":log,"explained_variance_score":var_score,"max_error":max
-}
+
 class Support_Regressor:
-    def __init__(self,train_features=[[1],[2],[3],[4],[5],[6]],training_labels=[1,4,9,16,25,36],testing_features=[[7],[8],[9],[10],[11],[12]],testing_labels=[49,64,81,100,121,144],hyperparams=None,validation_metric="root_mean_squared_error"):
+    def __init__(self,train_features=[[1],[2],[3],[4],[5],[6]],training_labels=[1,4,9,16,25,36],testing_features=[[7],[8],[9],[10],[11],[12]],testing_labels=[49,64,81,100,121,144],hyperparams={},validation_metric="root_mean_squared_error"):
         self.model = SVR(**hyperparams)
         self.model.fit(train_features, training_labels)
         self.validation_metric = validation_metric
