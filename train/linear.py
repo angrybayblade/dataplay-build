2bdae36ef9b9b949b2b314e4909f0831ab9598f4
jithuazeez <jithuazeez@gmail.com>
2019-08-24 16:21:57 +0530

    linear model added

--- train/linear.py
+++ train/linear.py
@@ -1,10 +1,18 @@
 from sklearn.linear_model import LinearRegression
+from validation import *
+
+evals={
+"root_mean_squared_error":rmse,"mean_squared_error":mse,"mean_absolute_error":mae,"r2_score":r2,"mean_squared_log_error":log,"explained_variance_score":var_score,"max_error":max
+}
+
 
 class Linear:
-    def __init__(self,train_features,training_labels,testing_features,testing_labels,hyperparams=None,validation_matric=None):
-        self.model = LinearRegression(**hyperparams)
-        #self.model.fit(train_features, training_labels)
+    def __init__(self,train_features=[[1],[2],[3],[4],[5],[6]],training_labels=[1,4,9,16,25,36],testing_features=[[7],[8],[9],[10],[11],[12]],testing_labels=[49,64,81,100,121,144],hyperparams=None,validation_metric="root_mean_squared_error"):
+        self.model = LinearRegression(hyperparams)
+        self.model.fit(train_features, training_labels)
         self.validation_metric = validation_metric
+        self.testing_features=testing_features
+        self.testing_labels=testing_labels
         self.hyperparams = hyperparams
         
     def predict(self,features):
@@ -14,14 +22,7 @@ class Linear:
         return matrix
 
     def validate(self):
-        if self.validation_matric:
-            if self.validation_metric == "rmse":
-                #perfrom rmse
-                return {
-                    "scores":[],
-                }
-        else:
-            #perform rmse
-            return {
-                
-            }
+        print (evals[self.validation_metric](self.testing_labels,self.predict(self.testing_features)))
+        
+lin=Linear()
+lin.validate()
--- train/validation.py
+++ train/validation.py
@@ -1,5 +1,26 @@
-def rmse(**kwargs):
+from sklearn.metrics import mean_squared_error,mean_absolute_error,r2_score,mean_squared_log_error
+from sklearn.metrics import explained_variance_score,max_error
+import numpy as np
+def rmse(y_true,y_pred):
     #perfrom rmse
-    return {
-        "result":[]
-    }
+    return(np.sqrt(mean_squared_error(y_true,y_pred)))
+
+def mse(y_true,y_pred):
+    return(mean_squared_error(y_true,y_pred))
+
+def mae(y_true,y_pred):
+    return(mean_absolute_error(y_true,y_pred))
+
+
+def r2(y_pred,y_true):
+    return(r2_score(y_true,y_pred))
+
+def log(y_true,y_pred):
+    return(mean_squared_log_error(y_true,y_pred))
+
+def var_score(y_true,y_pred):
+    return(explained_variance_score(y_true,y_pred))
+
+def max(y_true,y_pred):
+    return(max_error(y_true,y_pred))
+
