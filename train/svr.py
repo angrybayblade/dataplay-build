724be6290fea5eb2af7719fae68031267a05ba32
jithuazeez <jithuazeez@gmail.com>
2019-08-24 17:03:01 +0530

    svr model added

--- train/svr.py
+++ train/svr.py
@@ -1,13 +1,19 @@
 from sklearn.svm import SVR
 from validation import *
 
-
-class Linear:
-    def __init__(self,train_features,training_labels,testing_features,testing_labels,hyperparams=None,validation_metric=None):
-        self.model = sv(**hyperparams)
-        #self.model.fit(train_features, training_labels)
+evals={
+"root_mean_squared_error":rmse,"mean_squared_error":mse,"mean_absolute_error":mae,"r2_score":r2,"mean_squared_log_error":log,"explained_variance_score":var_score,"max_error":max
+}
+class Support_Regressor:
+    def __init__(self,train_features=[[1],[2],[3],[4],[5],[6]],training_labels=[1,4,9,16,25,36],testing_features=[[7],[8],[9],[10],[11],[12]],testing_labels=[49,64,81,100,121,144],hyperparams=None,validation_metric="root_mean_squared_error"):
+        self.model = SVR(**hyperparams)
+        self.model.fit(train_features, training_labels)
         self.validation_metric = validation_metric
         self.hyperparams = hyperparams
+        self.train_features=train_features
+        self.training_labels=training_labels
+        self.testing_features=testing_features
+        self.testing_labels=testing_labels
         
     def predict(self,features):
         return self.model.predict(features)
@@ -16,12 +22,6 @@ class Linear:
         return matrix
 
     def validate(self):
-        if self.validation_matric:
-            if self.validation_metric == "rmse":
-                #perfrom rmse
-                return rmse()
-        else:
-            #perform rmse
-            return {
-                
-            }
+         print (evals[self.validation_metric](self.testing_labels,self.predict(self.testing_features)))
+sv=Support_Regressor()
+sv.validate()
